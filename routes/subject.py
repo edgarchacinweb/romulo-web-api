6a7bffc7eff67d8d9660343086616babd19b8bef
Nat123-g <natasha.gainza99@gmail.com>
2026-04-02 17:38:47 -0400

    fix(asistencia): elimina materias duplicadas en selector del docente

--- routes/subject.py
+++ routes/subject.py
@@ -61,13 +61,11 @@ def teacher_subjects():
         # d."DatosPersonaId" en la tabla Docente
         # u."DatosPersona" en la tabla Usuario
         query = """
-            SELECT DISTINCT m."MateriaId", m."Nombre" AS "MateriaNombre", cu."Grado", cu."CursoId"
+            SELECT DISTINCT m."MateriaId", m."Nombre"
             FROM "Materia" m
             INNER JOIN "DocenteMateria" dm ON m."MateriaId" = dm."MateriaId"
             INNER JOIN "Docente" d ON dm."DocenteId" = d."DocenteId"
             INNER JOIN "Usuario" u ON d."DatosPersonaId" = u."DatosPersona"
-            INNER JOIN "MateriaHorasAcademicas" mh ON m."MateriaId" = mh."MateriaId"
-            INNER JOIN "Curso" cu ON mh."CursoId" = cu."CursoId"
             WHERE m."Activo" = true AND u."UsuarioId" = %s;
         """
         
@@ -76,9 +74,7 @@ def teacher_subjects():
 
         return jsonify([{
             "MateriaId": s[0],
-            "Nombre": s[1],
-            "Grado": s[2],
-            "CursoId": s[3]
+            "Nombre": s[1]
         } for s in rows]), 200
     except Exception as err:
         conn.rollback()
